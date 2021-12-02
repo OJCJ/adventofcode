1dd669aa5190c9f59dbcd1e2511e3957e6fb96ae
Oscar J. C. Johansen <44866812+OJCJ@users.noreply.github.com>
2021-12-02 11:27:06 +0100

    Completed day 1 part 2

--- Day 1/Day 1 - part 2.py
+++ Day 1/Day 1 - part 2.py
@@ -1,9 +1,14 @@
 with open("data.txt", "r") as f:
 	data = f.readlines()
 	increases = 0
+	measurement_windows = []
 	for i, val in enumerate(data):
 		data[i] = int(val)
+	while len(data) > 0:
+		measurement_windows.append(sum(data[:min(len(data),3)]))
+		data = data[1:]
+	for i, val in enumerate(measurement_windows):
 		if i > 0:
-			if data[i] > data[i-1]:
+			if val > measurement_windows[i-1]:
 				increases += 1
 	print(increases)
